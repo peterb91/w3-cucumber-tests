80234d5ad54847f0ab9f3746e0a5d72722b2333d
peterb91 <piotrekbednarz@gmail.com>
2021-10-24 16:29:44 +0200

    add cucumber structure and dependencies

---
+++ src/test/java/w3/org/runners/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package w3.org.runners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("w3/org")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
